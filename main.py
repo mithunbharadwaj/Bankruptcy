c80e783478981b63c1e02142b0c5f1ea7cd1a190
mithunbharadwaj <mithunshivashankar@gmail.com>
2021-02-02 20:41:13 +0100

    Working MI Models

--- main.py
+++ main.py
@@ -9,25 +9,58 @@ data = pd.read_csv("data.csv")
 X = np.array(data.drop(["Class"],axis=1))
 y = np.array(data["Class"])
 
+def rep(x):
+    if x == 'NB':
+        return 0
+    else:
+        return 1
+
+def rep_column(x):
+    if x == 'P':
+        return 1
+    elif x == 'A':
+        return 2
+    else:
+        return 3
+
+
+data["Class"] = data["Class"].map(rep)
+data["Industrial Risk"] = data["Industrial Risk"].map(rep_column)
+data["Industrial Risk.1"] = data["Industrial Risk.1"].map(rep_column)
+data[" Financial Flexibility"] = data[" Financial Flexibility"].map(rep_column)
+data["Credibility"] = data["Credibility"].map(rep_column)
+data["Competitiveness"] = data["Competitiveness"].map(rep_column)
+data["Operating Risk"] = data["Operating Risk"].map(rep_column)
+print(data.head())
+data.to_csv("data_new.csv",index=None,header=True)
+
+
+
 #Name columns
 #data.columns = ["Industrial Risk","Industrial Risk"," Financial Flexibility","Credibility","Competitiveness","Operating Risk","Class"]
 
 #store file as data.csv
 
-data.to_csv("data.csv",index=None,header=True)
-#
+data.to_csv("data_new.csv",index=None,header=True)
 
+data = pd.read_csv("data_new.csv")
+
+X = np.array(data.drop(["Class"],axis=1))
+
+y = np.array(data["Class"])
+
+#Perform data split 90%
 [X_train,X_test,y_train , y_test] = train_test_split(X, y, test_size=0.1, random_state=0)
 
-#SVC Classifier
-Classifier = SVC(kernel='_linear_')
+# Build SVC Classifier
+Classifier = SVC(kernel='linear')
 model = Classifier.fit(X_train,y_train)
 accu = model.score(X_train,y_train)
 
 print("Accuracy of SVC: " , accu)
 
-#Logistic Regression
-Classifier = LogisticRegression()
+# Build Logistic Regression
+Classifier = LogisticRegression(solver='liblinear')
 model = Classifier.fit(X_train,y_train)
 accu = model.score(X_train,y_train)
 
